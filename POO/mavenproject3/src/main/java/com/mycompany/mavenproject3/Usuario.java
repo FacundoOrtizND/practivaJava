d5dbdb3ac0ee5cf9ecc1f9f344f167098dde0e57
FacundoOrtizND <ofacundo01@gmail.com>
2026-04-09 11:38:16 -0300

    Add files via upload

---
+++ POO/mavenproject3/src/main/java/com/mycompany/mavenproject3/Usuario.java
@@ -0,0 +1,14 @@
+package com.mycompany.mavenproject3;
+
+public class Usuario {
+
+    String nombre;
+
+    public Usuario(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+}
